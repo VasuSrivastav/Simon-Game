c7f9885201e4ec44a27c8ddac6a0a34928580b8d
VasuSrivastav <115205203+VasuSrivastav@users.noreply.github.com>
2024-03-29 01:25:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function checkAnswer(currentLevel) {
   } else {
     playSound("wrong");
     $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Press Here or Any Key to Restart");
+    $("#level-title").text("Game Over, Click Here or Any Key to Restart");
 
     setTimeout(function () {
       $("body").removeClass("game-over");
